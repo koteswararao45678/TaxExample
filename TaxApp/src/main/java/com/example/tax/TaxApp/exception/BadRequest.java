7e7f7ebc8c97a3f1002db31fc746a19b142a9896
kotiramanadham1992 <135738330+kotiramanadham1992@users.noreply.github.com>
2023-06-13 15:04:11 +0530

    Update BadRequest.java

--- TaxApp/src/main/java/com/example/tax/TaxApp/exception/BadRequest.java
+++ TaxApp/src/main/java/com/example/tax/TaxApp/exception/BadRequest.java
@@ -6,7 +6,7 @@ package com.example.tax.TaxApp.exception;
 public class BadRequest extends Exception {
     private static final long serialVersionUID = 1L;
 
-    public BadRequest(int i, String msg) {
+    public BadRequest(String msg) {
         super(msg);
     }
 }
