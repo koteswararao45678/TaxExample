4d3a4a9c487f31b6463d1c5cf3b96bd102499f96
kotiramanadham1992 <135738330+kotiramanadham1992@users.noreply.github.com>
2023-06-13 15:06:33 +0530

    Update GlobalExceptionHandler.java

--- TaxApp/src/main/java/com/example/tax/TaxApp/exception/GlobalExceptionHandler.java
+++ TaxApp/src/main/java/com/example/tax/TaxApp/exception/GlobalExceptionHandler.java
@@ -7,29 +7,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
+public class GlobalExceptionHandler {
 
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-
-    @ExceptionHandler(BadRequest.class)
-
-    public ResponseEntity<ErrorDetails> userNotFoundException(BadRequest ex) {
-
-        ErrorDetails errorModel = new ErrorDetails(0, ex.getMessage());
-
-        return new ResponseEntity<ErrorDetails>(errorModel, HttpStatus.NOT_FOUND);
-
-    }
-
-    @ExceptionHandler(Exception.class)
-
-    public ResponseEntity<?> globleExcpetionHandler(Exception ex) {
-
-        ErrorDetails errorModel = new ErrorDetails(0, ex.getMessage());
-
-        return new ResponseEntity<>(errorModel, HttpStatus.INTERNAL_SERVER_ERROR);
-
+    @ExceptionHandler(value = BadRequest.class)
+    public ResponseEntity<CustomErrorResponse> badRequest(Exception e) {
+        CustomErrorResponse error = new CustomErrorResponse(400, e.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }
